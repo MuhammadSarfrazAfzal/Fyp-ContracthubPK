78a9d84536f44733228b75b8a1fc64d6135cf216
MuhammadSarfrazAfzal <sarfrazafzal790@gmail.com>
2026-01-01 21:54:21 +0500

    created contract report section

--- Source/client/src/App.jsx
+++ Source/client/src/App.jsx
@@ -4,12 +4,14 @@ import Dashboard from './components/Dashboard/Dashboard'
 import NavBar from './components/NavBar/NavBar'
 import MyContracts from './components/My Contracts/MyContracts'
 import NewContract from './components/My Contracts/New Contracts/NewContracts'
+import ContractsByStatus from './components/Reports/contract-report'
 function App() {
   return (
     <>
         <NavBar></NavBar> 
         <MyContracts></MyContracts>
         <NewContract></NewContract>
+        <ContractsByStatus></ContractsByStatus>
     </>
   )
 }
